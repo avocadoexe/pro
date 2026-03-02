a7e803900afb9d5133075183851873c4db343ada
avocadoexe <avocadotv.exe@gmail.com>
2026-03-02 18:55:24 +0530

    Add object class to count instances

---
+++ object.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class objects{
+static int x;
+objects() {
+x=x+1; }
+public static void main(String []args){
+objects a=new objects();
+objects b=new objects();
+objects c=new objects();
+System.out.println("Number of objects : "+x); }
